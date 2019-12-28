124df24f1fb11517728228429dac49ca96cbebf5
Milo Knowles <mknowles@mit.edu>
2019-12-28 12:22:50 -0500

    Tensorboard script

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,4 +1,4 @@
 # Runs a tensorboard server to view training progress.
 # NOTE(milo): Had to add export PATH="$PATH:/home/milo/.local/bin" to get this executable.
 
-tensorboard --logdir /home/milo/pokerbots-2020/training_logs/$1 --bind_all --port 6006
+tensorboard --logdir /home/milo/pokerbots-2020/training_logs/$1 --port 6006
