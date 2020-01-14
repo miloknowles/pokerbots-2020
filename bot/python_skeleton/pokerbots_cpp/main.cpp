774c9cb2fed0abe6a810805c2e1271c5f9582a4f
Milo Knowles <mknowles@mit.edu>
2020-01-14 06:44:51 -0800

    More cpp

--- bot/python_skeleton/pokerbots_cpp/main.cpp
+++ bot/python_skeleton/pokerbots_cpp/main.cpp
@@ -2,7 +2,7 @@
 
 
 int main(int argc, char const *argv[]) {
-  int N = 1000000;
+  int N = 10000;
 
   for (int i = 0; i < N; ++i) {
     const std::string query = "AcTd:QsQd";
@@ -11,4 +11,10 @@ int main(int argc, char const *argv[]) {
     const float ev = pb::PbotsCalcEquity(query, board, dead, 1);
     // std::cout << ev << std::endl;
   }
+
+  pb::PermutationFilter pf(5000);
+
+  const pb::Permutation& perm = pf.PriorSample();
+  const double prior = pf.ComputePrior(perm);
+  std::cout << "Prior: " << prior << std::endl;
 }
