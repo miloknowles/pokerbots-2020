87ffec650f796f1ed4ba89469f80f292b5c90697
Milo Knowles <mknowles@mit.edu>
2020-01-23 08:26:57 -0800

    No comments inline for bash

--- experiments/cfr_small.sh
+++ experiments/cfr_small.sh
@@ -1,6 +1,6 @@
 python ../train_cfr.py --EXPERIMENT_NAME mc_cfr_03 \
-  --NUM_TRAVERSE_WORKERS 28 \       # 28 works well on 32 core machine.
-  --NUM_TRAVERSALS_PER_ITER 560 \   # Multiple of num workers.
-  --NUM_TRAVERSALS_EVAL 10 \        # Fewer needed with no external sampling.
+  --NUM_TRAVERSE_WORKERS 28 \
+  --NUM_TRAVERSALS_PER_ITER 560 \
+  --NUM_TRAVERSALS_EVAL 10 \
   --TRAVERSE_DEBUG_PRINT_HZ 5 \
   --NUM_CFR_ITERS 500 \
