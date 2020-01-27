9a990ee1dbffc30bb803e233bd72516075d1f638
Milo Knowles <mknowles@mit.edu>
2020-01-27 14:13:43 -0500

    Cleanup, new SSH

--- pypokergui/run_game.sh
+++
@@ -1,1 +0,0 @@
-pypokergui serve ./poker_conf.yaml --port 8000 --speed fast
---
+++ ssh_trainer_01.sh
@@ -0,0 +1,3 @@
+gcloud compute ssh --project robustroboticsgroup-225320 \
+  --zone us-east1-b milo-trainer-01 \
+  -- -L 8080:localhost:8080
