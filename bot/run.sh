ef1d94c884d0d981d6c1ebaab74901aaea8893f3
Milo Knowles <mknowles@mit.edu>
2020-01-28 18:17:41 -0500

    Speed up build

--- bot/run.sh
+++ bot/run.sh
@@ -1,4 +1,10 @@
 # source setup.sh
 export LD_LIBRARY_PATH=`pwd`/pokerbots_cpp/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/build/pokerbots_cpp/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH
+
 echo $LD_LIBRARY_PATH
-cd build && ./cpp_player --port $1
+
+cd build
+chmod +x cpp_player
+./cpp_player --port $1
