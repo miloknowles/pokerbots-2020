e84d7790df39ad7dd921ddd784e570c5afbb4f94
Milo Knowles <mknowles@mit.edu>
2020-01-28 16:34:53 -0500

    Put kmeans in separate dir

--- bot/pokerbots_cpp/cfr_main_kmeans.cpp
+++ bot/pokerbots_cpp/cfr_main_kmeans.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char const *argv[]) {
   pb::cfr::Options opt;
-  opt.NUM_TRAVERSALS_PER_ITER = 10;
-  opt.NUM_TRAVERSALS_EVAL = 10;
+  // opt.NUM_TRAVERSALS_PER_ITER = 10;
+  // opt.NUM_TRAVERSALS_EVAL = 10;
   // opt.BUCKET_FUNCTION = pb::cfr::Bucket_10_16;
   opt.EXPERIMENT_NAME = "MC_CFR_KMEANS";
-  opt.EXPERIMENT_PATH = "/home/milo/pokerbots-2020/cfr/" + opt.EXPERIMENT_NAME + "/";
+  opt.EXPERIMENT_PATH = "/home/milo/kmeans/pokerbots-2020/cfr/" + opt.EXPERIMENT_NAME + "/";
   pb::cfr::CfrTrainer trainer(opt);
   trainer.Run();
   std::cout << "Finished running CFR" << std::endl;
