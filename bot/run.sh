4c50e745fc3573b5d374c6c2fa7d5cecc318d83c
Milo Knowles <mknowles@mit.edu>
2020-01-29 09:53:33 -0500

    Got prebuild working

--- bot/run.sh
+++ bot/run.sh
@@ -1,7 +1,7 @@
 # source setup.sh
 export LD_LIBRARY_PATH=`pwd`/pokerbots_cpp/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/build/pokerbots_cpp/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH
 
 echo $LD_LIBRARY_PATH
 
