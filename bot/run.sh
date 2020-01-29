186a05827a9b31641cedef1f6e65783c0326e7dd
Milo Knowles <mknowles@mit.edu>
2020-01-29 11:39:42 -0500

    LD_LIBRARY_PATH

--- bot/run.sh
+++ bot/run.sh
@@ -1,5 +1,5 @@
 # source setup.sh
-export LD_LIBRARY_PATH=`pwd`/pokerbots_cpp/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/build/:`pwd`/build/pokerbots_cpp/:`pwd`/pokerbots_cpp/:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 cd build
 chmod +x cpp_player
